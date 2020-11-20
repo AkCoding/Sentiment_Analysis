5ca9ab1d031531eba35148b9bd9eb58f2b364841
Arbaaz <khanarbaj45@.com>
2020-11-20 16:11:16 +0530

    Text Review Sentiment_Analysis using Textblob Library

---
+++ analysis.py
@@ -0,0 +1,6 @@
+from textblob import TextBlob
+
+text = TextBlob('it was a wonderful movie, i liked it')
+print(text.sentences)
+print('Positive{}'.format(text.sentiment.polarity))
+print('Negativity{}'.format(text.sentiment.subjectivity))
